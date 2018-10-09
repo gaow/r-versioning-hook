bdf3d6b8c9da161fd064aaf60df4bbc0f8a16ef5
Gao Wang <gaow@uchicago.edu>
2018-10-09 18:13:27 -0500

    Change exit code

--- inst/misc/pre-commit.sh
+++ inst/misc/pre-commit.sh
@@ -6,7 +6,6 @@
 # To use this script, copy it to the .git/hooks directory of your
 # local repository to filename `pre-commit`, and make it executable.
 #
-set -e
 ROOT_DIR=`git rev-parse --show-toplevel`
 MSG="[WARNING] Auto-versioning disabled because string 'Version: x.y.z.r' cannot be found in DESCRIPTION file."
 GREP_REGEX='^Version: [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'
@@ -16,7 +15,6 @@ if [[ -z `git diff HEAD` ]] || [[ ! -f $ROOT_DIR/DESCRIPTION ]]; then
     exit 0
 elif [[ -z `grep "$GREP_REGEX" $ROOT_DIR/DESCRIPTION` ]]; then
     echo "$MSG"
-    exit 0
 else
     REV_ID=`git log --oneline | wc -l`
     REV_ID=`printf "%04d\n" $((REV_ID+1))`
@@ -24,5 +22,4 @@ else
     echo "Version string bumped to revision $REV_ID on $DATE"
     sed -i "s/$SED_REGEX/Version: \1.$REV_ID/" $ROOT_DIR/DESCRIPTION
     sed -i "s/^Date: .*/Date: $DATE/" $ROOT_DIR/DESCRIPTION
-    exit 0
 fi
